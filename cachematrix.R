2f464d9269c9923d2534b5d0c159ef5d653a2459
gerrycrprog <gerry.conyngham@gmail.com>
2016-04-10 20:51:19 +0100

    Added files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions are designed to ensure that the inverse
+## of a mtrix is not calculated more than once. They do this by 
+## caching the inverse of the matrix the first time the inverse 
+## is calcualted and callig the cache of the inverse if inverse
+## is required again. 
 
-## Write a short comment describing this function
+## makecachematrix() creates a list which stores four functions that
+## set and get a matrix and set and get the inverse of a matrix. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        Inv <- NULL
+        ## Define function setm() - Sets the Matrix    
+        setm <- function(yt) {
+                x <<- yt
+                In <<- NULL
+        }
+        ## Define functions getm(), setinv(), getinv() : 
+        ## getm()   - Gets the Matrix created by Set
+        ## setinv() - Sets the Inverse of the Matrix
+        ## getinv() - Gets Matrix Inverse set by Setinv
+        getm <- function() x
+        setinv <- function(solve) Inv <<- solve
+        getinv <- function() Inv
+  
+        ## Function returns list containing the four functions Setm(),getm(), 
+        ## setinv() and getinv() 
+        list(setm = setm, getm = getm,
+        setinv = setinv,
+        getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function checks if Inverse calculated previously. If inverse already 
+## calcualted, it is retrieved from cache, otherwise inverse calculated 
+## using solve function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+        ## Cached Inverse retrieved using getinv(). If non NULL matrix cached inverse 
+        ## returned. 
+        Inv <- x$getinv()                    
+        if(!is.null(Inv)) {
+                message("Using cached inverse")
+        return(Inv)
+        }
+        ## Matrix data retrieved using getm() and Inverse calculated using solve()
+        ## and returned. Inverse also stored in Cache using setinv().
+        data <- x$getm()
+        Inv <- solve(data, ...)
+        x$setinv(Inv)
+        Inv
 }
